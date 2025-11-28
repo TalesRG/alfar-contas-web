4ba2d327f94753a0647e33a8aa2b43e74102504c
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-11-28 00:45:58 -0300

    feat: adiciona layout

---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,24 @@
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    RouterOutlet,
+    NgIf
+  ],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+  sidebarOpen = signal(true);
+
+  toggleSidebar() {
+    this.sidebarOpen.set(true)
+  }
+
+  closeSidebar() {
+    this.sidebarOpen.set(false)
+  }
+}
