da6f99a2ea5543106ea08cd36d77a5c616d2b52a
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-11-28 00:45:22 -0300

    criando projeto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CardPayment} from './components/card-payment/card-payment';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : 'teste', component : CardPayment},
+];
